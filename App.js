f56428da31a4b8d70cabfc31c97f0e3c11d7ceb7
James McGill <contact@jamesmcgill.co.uk>
2018-06-24 09:15:13 +0100

    Created basic structure of the components.
    Built container with state obj for presentational components. Created 
    the presentaional components to add a bookmark, display the bookmarks 
    and a button to handle adding, editing and deleting bookmarks.

--- App.js
+++ App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => (
-  <div>
-    Hello World
-  </div>
+const Button = ({ type, className, handleClick, text }) => (
+  <button type={type} className={className} onClick={handleClick}>
+    {text}
+  </button>
 );
 
+const Bookmarks = ({ bookmarksArr }) => bookmarksArr.map(bookmark => (
+  <li key={bookmark}>
+    {bookmark}
+  </li>
+));
+
+const AddBookmark = ({ value, handleChange, handleSubmit, display }) => (
+  <form onSubmit={handleSubmit} style={{ display }}>
+    <label>
+      Enter Website Address
+    </label>
+    <input type="text" value={value} onChange={handleChange} />
+  </form>
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bookmarks: [],
+      formValue: '',
+      AddBookmarkDisplay: 'none',
+    };
+  }
+}
+
 export default App;
+
+// App component
+// Hyperlinks list component
+// Add link form component
+// Button component for Add, Edit & Delete functionality
